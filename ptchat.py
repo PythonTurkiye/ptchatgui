f6a238faa44296ecf43972bc4a7ba560c0a50d3f
Sefa Akgümüş <sefisakgms@gmail.com>
2015-04-26 22:51:57 +0300

    Dosya ilk commit duzenleme yapildi

--- ptchat.py
+++ ptchat.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ptchat.ui'
-#
-# Created: Sun Apr 26 15:13:45 2015
-#      by: PyQt4 UI code generator 4.10.4
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt4 import QtCore, QtGui
 import sys
 import socket
